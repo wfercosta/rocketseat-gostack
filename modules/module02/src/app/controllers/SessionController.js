524db83fb0f4e49d2add5af69c2d427d888f27ee
Wander Costa <wanderfernando@gmail.com>
2020-03-28 19:20:59 -0300

    Adds /sessions route implementation in order to generate jwt token once user is successfuly logged in

---
+++ modules/module02/src/app/controllers/SessionController.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+
+import config from '../../config/auth';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ message: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ message: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: { id, name, email },
+      token: jwt.sign({ id }, config.secret, {
+        expiresIn: config.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
---
+++ modules/module02/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'df15cc954b4a65ff3bf2be7e2ced1362',
+  expiresIn: '7d',
+};
