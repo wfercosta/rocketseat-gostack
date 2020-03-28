9222ff263615032e37ac2a877618973c317b4899
Wander Costa <wanderfernando@gmail.com>
2020-03-28 15:04:30 -0300

    Adds testing route in order to validate the sequilize and models configuration

--- modules/module02/src/routes.js
+++ modules/module02/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Wander Fernando Costa',
+    email: 'wanderfernando@gmail.com',
+    password_hash: '123123123123',
+  });
+  return res.json(user);
 });
 
 export default routes;
