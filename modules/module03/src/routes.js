45135325db8b8ba5c39703a63e1926c87f46e4b1
Wander Costa <wanderfernando@gmail.com>
2020-03-30 20:03:35 -0300

    Adds fix to migration of users, configuration of static resources and implementation of upload feature

--- modules/module03/src/routes.js
+++ modules/module03/src/routes.js
@@ -4,8 +4,10 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,8 +18,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 
 export default routes;
