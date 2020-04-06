926ef8aba3a43bef620332245262de130e5e4ce7
Wander Costa <wanderfernando@gmail.com>
2020-04-06 20:02:24 -0300

    [capstone] Fix #1 - Adds deliverymen table migrations

---
+++ capstone/fastfeet-backend/src/database/migrations/20200406222944-create-deliverymen.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('deliverymen', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(60),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(60),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('deliverymen');
+  },
+};
