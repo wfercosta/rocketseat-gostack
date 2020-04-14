61d6d636d59ce68dc7bf7c5173d3a3b613d7ec63
Wander Costa <wanderfernando@gmail.com>
2020-04-14 06:40:25 -0300

    Adds notification to module09 exercice

---
+++ modules/module09/src/components/Header/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+import Notifications from '~/components/Notifications';
+
+import { Container, Content, Profile } from './styles';
+
+import logo from '~/assets/logo-purple.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+        <aside>
+          <Notifications />
+          <Profile>
+            <div>
+              <strong>Wander Fernando Costa</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Wander Fernando Costa"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
