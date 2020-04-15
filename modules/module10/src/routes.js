01d6d57698b77299bf86eec01af5fca6d3ee0c2a
Wander Costa <wanderfernando@gmail.com>
2020-04-14 21:29:44 -0300

    Adds initial implementation mobile

---
+++ modules/module10/src/routes.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          headerBackTitleVisible: false,
+          headerBackTitle: false,
+          headerTitleAlign: 'center',
+          headerTintColor: '#FFF',
+          headerStyle: {
+            backgroundColor: '#7159c1',
+          },
+        }}
+      >
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
